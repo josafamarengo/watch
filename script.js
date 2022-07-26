cbd832d5b43aa01c92d89e08ef9b55cc49617559
josafamarengo <josafabmarengo@gmail.com>
2022-07-26 20:10:49 -0300

    feat: first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+const updateClock = () => {
+    const date = new Date();
+    const seconds = date.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = date.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360);
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = date.getHours();
+    const hoursDegrees = (((hours * 5) / 60) * 360);
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+};
+
+updateClock();
+
+setInterval(updateClock, 1000);
